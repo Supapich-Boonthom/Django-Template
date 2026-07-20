50c837f56138d6525b35ad4adf123e5a5a46e9fb
Supapich-Boonthom <pattarawadee001425@gmail.com>
2026-07-20 09:55:33 +0700

    commit

--- web/views.py
+++ web/views.py
@@ -1,10 +1,14 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Home Page")
 
-def contact(request):
-    return HttpResponse("Contact Page")
+def home(request):
+    return render(request, "index.html")
+
 
 def about(request):
-    return HttpResponse("About Page")
+    return render(request, "about.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
