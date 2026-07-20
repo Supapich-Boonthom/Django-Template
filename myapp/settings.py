bdbf555bb9fb3b8ca7884d620d18cc7f4a24b290
Supapich-Boonthom <pattarawadee001425@gmail.com>
2026-07-20 10:19:12 +0700

    first commit

--- myapp/settings.py
+++ myapp/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "myapp.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ["templates", ],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -119,6 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "statics",]
 STATIC_ROOT = BASE_DIR / "staticfiles"
