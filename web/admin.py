cf167839917853d8ef5c2b517bb0ef3e7c907d16
Supapich-Boonthom <pattarawadee001425@gmail.com>
2026-08-10 10:35:43 +0700

    commit10

--- web/admin.py
+++ web/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Student
+from .models import Student, Major
+
 
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('st_id', 'prefix_name', 'fname', 'lname')
+    list_display = ("st_id", "prefix_name", "fname", "lname", "major")
+
+
+class MajorAdmin(admin.ModelAdmin):
+    list_display = ("mj_name",)
+
 
 admin.site.register(Student, StudentAdmin)
+admin.site.register(Major, MajorAdmin)
--- web/models.py
+++ web/models.py
@@ -10,14 +10,30 @@ PREFIX_NAME = (
     ("นาง", "นาง"),
 )
 
+
 class Student(models.Model):
     prefix_name = models.CharField(max_length=10, choices=PREFIX_NAME, default="นาย")
     st_id = models.CharField(max_length=12, unique=True)
     fname = models.CharField(max_length=100, blank=False)
     lname = models.CharField(max_length=100, blank=False)
+    major = models.ForeignKey('Major', on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.fname + " " + self.lname
 
     def get_absolute_url(self):
-        return reverse("Student_detail", kwargs={"pk": self.pk})
+        return reverse("Student_detail", kwargs={"pk": self.pk})
+
+
+class Major(models.Model):
+    mj_name = models.CharField(max_length=100, blank=False)
+
+    class Meta:
+        verbose_name = "Major   "
+        verbose_name_plural = "Majors"
+
+    def __str__(self):
+        return self.mj_name
+
+    def get_absolute_url(self):
+        return reverse("Major_detail", kwargs={"pk": self.pk})
